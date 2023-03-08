67702cfd06f0a4065024b031d76519114f83ee46
Simon Yong <51854826+simony666@users.noreply.github.com>
2023-03-08 17:08:06 +0800

    product page

---
+++ assets/js/product.js
@@ -0,0 +1,22 @@
+$(document).ready(function(){
+
+    function store(){
+        console.log("store!");
+    }
+
+    //check out button
+    $(".price-btn").click(function(){
+        console.log("check out btn CLick!");
+    });
+
+    //add to cart button
+    $(".cart-btn").click(function(){
+        console.log("add to cart btn CLick!");
+    });
+
+    //buy button
+    $(".buy-btn").click(function(){
+        console.log("buy  btn CLick!");
+    });
+  
+  });
